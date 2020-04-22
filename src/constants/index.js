d7550bb241885d6712b385aff7ce34a44b0056fd
stevenlmcgraw <stevenlmcgraw@gmail.com>
2020-04-22 13:42:24 -0500

    Adds manifest.yml

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,6 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:9191';
+//export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:9191';
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://saturn-hotdog-super-calculator-backend.cfapps.io';
+
 export const ACCESS_TOKEN = 'accessToken';
 
 export const MIN_USERNAME_LENGTH = 6;
